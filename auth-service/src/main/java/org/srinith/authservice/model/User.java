d0b2e324a58bbffe87c54a30fc140f9598b1be22
Srinith Reddy <srinithreddy2002@gmail.com>
2026-03-05 17:15:06 -0600

    Add auth-service with PostgreSQL database setup, SQL schema, and Docker configuration

---
+++ auth-service/src/main/java/org/srinith/authservice/model/User.java
@@ -0,0 +1,28 @@
+package org.srinith.authservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
